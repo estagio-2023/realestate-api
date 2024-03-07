1d2c4083647f8edadd3ca29fd24eb55080ce4609
jonathan.d.osorio@cgi.com <jonathan.d.osorio@cgi.com>
2024-03-07 21:37:25 +0000

    Created database connection using Npgsql

--- AppCasasAPI/AppCasasAPI/Controllers/AppCasasController.cs
+++ AppCasasAPI/AppCasasAPI/Controllers/AppCasasController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Security;
 
 namespace AppCasasAPI.Controllers
 {
--- AppCasasAPI/AppCasasAPI/Program.cs
+++ AppCasasAPI/AppCasasAPI/Program.cs
@@ -1,8 +1,14 @@
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("AppCasasDB");
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
