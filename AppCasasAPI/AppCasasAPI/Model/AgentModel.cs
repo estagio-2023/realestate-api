daa2efd3323b4613798383c545c8a04a08d1c595
Jonathan Osório <154422201+jonathan-osorio@users.noreply.github.com>
2024-03-13 17:41:28 +0000

    Update AgentModel.cs - Cellphone to PhoneNumber

--- AppCasasAPI/AppCasasAPI/Model/AgentModel.cs
+++ AppCasasAPI/AppCasasAPI/Model/AgentModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Cellphone { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
     }
 }
