c2b26b04bc566e909f87586b2473ad4137a27573
jonathan.d.osorio@cgi.com <jonathan.d.osorio@cgi.com>
2024-03-14 17:19:58 +0000

    Created Repository and interface for the Reference Data, Controller and Dto Response

---
+++ AppCasasAPI/AppCasasAPI/Dto/Response/ReferenceDataResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using AppCasasAPI.Model;
+
+namespace AppCasasAPI.Dto.Response
+{
+    public class ReferenceDataResponseDto
+    {
+        public List<TypologyModel> TypologiesList { get; set; } = new();
+        public List<RealEstateTypeModel> RealEstateTypesList { get; set; } = new();
+        public List<CityModel> CitiesList { get; set; } = new();
+        public List<AmenitiesModel> AmenitiesList { get; set; } = new();
+
+    }
+}
---
+++ AppCasasAPI/AppCasasAPI/Repository/ReferenceDataRepository.cs
@@ -0,0 +1,89 @@
+﻿using Npgsql;
+using AppCasasAPI.Repository.Interfaces;
+using AppCasasAPI.Dto.Response;
+using AppCasasAPI.Model;
+
+namespace AppCasasAPI.Repository
+{
+    public class ReferenceDataRepository : IReferenceDataRepository
+    {
+        private readonly NpgsqlDataSource _dataSource;
+
+        public ReferenceDataRepository(NpgsqlDataSource dataSource) {
+            _dataSource = dataSource;
+        }
+
+        public async Task<ReferenceDataResponseDto> GetAllReferenceDataAsync()
+        {
+            ReferenceDataResponseDto refData = new();
+            using (var conn = await _dataSource.OpenConnectionAsync())
+            {
+                using var typologyQuerry = new NpgsqlCommand("SELECT * FROM tipologia;", conn);
+                var typologyReader = await typologyQuerry.ExecuteReaderAsync();
+
+                while (typologyReader.Read())
+                {                    
+                    var typologyModel = new TypologyModel();
+                    typologyModel.Description = typologyReader["descricao"].ToString();
+                    refData.TypologiesList.Add(typologyModel);
+                }
+
+                typologyReader.Close();
+
+
+                using var realEstateTypeQuerry = new NpgsqlCommand("SELECT * FROM tipo_imovel;", conn);
+                var realEstateTypeReader = await realEstateTypeQuerry.ExecuteReaderAsync();
+
+                while (realEstateTypeReader.Read())
+                {
+                    var realEstateTypeModel = new RealEstateTypeModel();
+                    realEstateTypeModel.Description = realEstateTypeReader["descricao"].ToString();
+                    refData.RealEstateTypesList.Add(realEstateTypeModel);
+                }
+
+                realEstateTypeReader.Close();
+
+                using var cityQuerry = new NpgsqlCommand("SELECT * FROM cidades;", conn);
+                var cityReader = await cityQuerry.ExecuteReaderAsync();
+
+                while (cityReader.Read())
+                {
+                    var citiesModel = new CityModel();
+                    citiesModel.Description = cityReader["descricao"].ToString();
+                    refData.CitiesList.Add(citiesModel);
+                }
+
+                cityReader.Close();
+
+                using var amenitiesQuerry = new NpgsqlCommand("SELECT * FROM cidades;", conn);
+                var amenitiesReader = await cityQuerry.ExecuteReaderAsync();
+
+                while (cityReader.Read())
+                {
+                    var amenitiesModel = new AmenitiesModel();
+                    amenitiesModel.Description = amenitiesReader["descricao"].ToString();
+                    refData.AmenitiesList.Add(amenitiesModel);
+                }
+
+                cityReader.Close();
+
+                return refData;
+            }
+        }
+
+
+        public async Task<string?> GetUserName()
+        {
+            using (var conn = await _dataSource.OpenConnectionAsync())
+            {
+                using var command = new NpgsqlCommand("SELECT nome FROM vendedor;", conn);
+                var execute = await command.ExecuteScalarAsync();
+                if (execute != null)
+                {
+                    return execute.ToString();
+                }
+                return null;
+            }            
+        }
+    }
+}
