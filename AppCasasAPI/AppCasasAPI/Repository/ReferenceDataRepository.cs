83ca0e70599f95a097f05f31964af300012eb326
jonathan.d.osorio@cgi.com <jonathan.d.osorio@cgi.com>
2024-03-19 15:04:22 +0000

    edited catch console writeline

--- AppCasasAPI/AppCasasAPI/Repository/ReferenceDataRepository.cs
+++ AppCasasAPI/AppCasasAPI/Repository/ReferenceDataRepository.cs
@@ -73,7 +73,7 @@ namespace AppCasasAPI.Repository
             }
             catch (Exception ex)
             {
-                Console.WriteLine("error");
+                Console.WriteLine(ex.Message);
             }
             return refData;
         }
