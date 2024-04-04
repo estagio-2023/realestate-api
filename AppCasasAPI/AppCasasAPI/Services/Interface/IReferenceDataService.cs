4854eca75c1befe7bd642bf5c147c295f65d2b4b
Silva096 <154329717+Silva096@users.noreply.github.com>
2024-04-04 16:17:01 +0100

    Revert " API - POST Reference Data endpoint"

--- AppCasasAPI/AppCasasAPI/Services/Interface/IReferenceDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿using AppCasasAPI.Dto.Response;
-
-namespace AppCasasAPI.Services.Interface
-{
-    public interface IReferenceDataService
-    {
-        public Task<AddReferenceDataResponseDto> AddReferenceDataAsync(string refDataType, ReferenceDataRequestDto refData);
-    }
-}
--- RealestateApi/Repository/Interfaces/IReferenceDataRepository.cs
+++ RealestateApi/Repository/Interfaces/IReferenceDataRepository.cs
@@ -5,9 +5,5 @@ namespace RealEstateApi.Repository.Interfaces
     public interface IReferenceDataRepository
     {
         Task<ReferenceDataResponseDto> GetAllReferenceDataAsync();
-        Task<AddReferenceDataResponseDto> AddTypologyReferenceDataAsync(string refDataType, ReferenceDataRequestDto refData);
-        Task<AddReferenceDataResponseDto> AddRealEstateTypeReferenceDataAsync(string refDataType, ReferenceDataRequestDto refData);
-        Task<AddReferenceDataResponseDto> AddCityReferenceDataAsync(string refDataType, ReferenceDataRequestDto refData);
-        Task<AddReferenceDataResponseDto> AddAmenityReferenceDataAsync(string refDataType, ReferenceDataRequestDto refData);
     }
 }
