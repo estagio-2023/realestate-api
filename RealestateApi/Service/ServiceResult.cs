8019ef090bd64d351e9a25f6fcefe9a3b12382c0
Jonathan Osório <jonathan.d.osorio@cgi.com>
2024-04-19 17:37:05 +0100

    Created Service Result class and ProblemTypes class

---
+++ RealestateApi/Service/ServiceResult.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstateApi.Service
+{
+    public class ServiceResult<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string ProblemTypes { get; set; }
+        public string AdditionalInformation { get; set; }
+        public T Result { get; set; }
+
+    }
+}
