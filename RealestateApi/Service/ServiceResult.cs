f89ce2712085ca0caba6282f99240b89e3e3040e
Jonathan Osório <jonathan.d.osorio@cgi.com>
2024-04-22 16:38:52 +0100

    Fixed property name and code cleaning

--- RealestateApi/Service/ServiceResult.cs
+++ RealestateApi/Service/ServiceResult.cs
@@ -3,9 +3,8 @@
     public class ServiceResult<T>
     {
         public bool IsSuccess { get; set; }
-        public string ProblemTypes { get; set; }
-        public string AdditionalInformation { get; set; }
+        public string ProblemType { get; set; }
+        public string AdditionalInformation { get; set; }   
         public T Result { get; set; }
-
     }
 }
