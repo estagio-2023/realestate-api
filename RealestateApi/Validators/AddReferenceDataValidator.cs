6355850d36b666c882318b38b484eec6bfa8ea52
Jonathan Osório <jonathan.d.osorio@cgi.com>
2024-04-30 09:21:35 +0100

    Deleted RefData Enums

--- RealestateApi/Validators/AddReferenceDataValidator.cs
+++ RealestateApi/Validators/AddReferenceDataValidator.cs
@@ -1,7 +1,5 @@
 ﻿using FluentValidation;
 using RealEstateApi.Dto.Request;
-using RealEstateApi.Enums;
-using RealEstateApi.Model;
 
 namespace RealEstateApi.Validators
 {
@@ -11,7 +9,6 @@ namespace RealEstateApi.Validators
         {
             RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
             RuleFor(x => x.Description).MinimumLength(5).WithMessage("Description must be at least 5 characters long");
-            RuleFor(x => x.Description).MaximumLength(50).WithMessage("Description must not exceed 50 characters");
             RuleFor(x => x.Description).MaximumLength(50).WithMessage("Description must not exceed 50 characters");
         }
     }
