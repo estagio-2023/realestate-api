1f5e8b96b3a16cd0c2dde3e69e5e9a58553d6d9d
Diogo Santos <diogomariasantos3@gmail.com>
2024-05-02 09:51:19 +0100

    Added validator ".NotEmpty()" to customer validations

--- RealestateApi/Validators/AddCustomerValidator.cs
+++ RealestateApi/Validators/AddCustomerValidator.cs
@@ -8,9 +8,9 @@ namespace RealEstateApi.Validators
     {
         public AddCustomerValidator()
         {
-            RuleFor(x => x.Name).Length(5, 100).WithMessage("Name is required.");
-            RuleFor(x => x.Email).Length(8, 150).EmailAddress().WithMessage("A valid email is required");
-            RuleFor(x => x.Password).Length(5, 150).WithMessage("Password is required.");
+            RuleFor(x => x.Name).Length(5, 100).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Email).Length(8, 150).EmailAddress().NotEmpty().WithMessage("A valid email is required");
+            RuleFor(x => x.Password).Length(5, 150).NotEmpty().WithMessage("Password is required.");
         }
     }
 }
