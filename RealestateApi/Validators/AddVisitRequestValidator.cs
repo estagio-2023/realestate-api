debd125a5056199710bb3afb4015ae94a07d565b
Diogo Santos <diogomariasantos3@gmail.com>
2024-05-28 11:37:34 +0100

    Updated hour validator format to HH:MM

--- RealestateApi/Validators/AddVisitRequestValidator.cs
+++ RealestateApi/Validators/AddVisitRequestValidator.cs
@@ -20,10 +20,10 @@ namespace RealEstateApi.Validators
                 .Matches(@"^\d{4}-\d{2}-\d{2}$").WithMessage("Date must be in the format YYYY-MM-DD.");
             RuleFor(x => x.StartTime)
                 .NotEmpty()
-                .Matches(@"^(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)$").WithMessage("Start time must be in the format HH:MM:SS.");
+                .Matches(@"^(?:[01]\d|2[0-3]):(?:[0-5]\d)$").WithMessage("Start time must be in the format HH:MM.");
             RuleFor(x => x.EndTime)
                 .NotEmpty()
-                .Matches(@"^(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)$").WithMessage("End time must be in the format HH:MM:SS.");
+                .Matches(@"^(?:[01]\d|2[0-3]):(?:[0-5]\d)$").WithMessage("End time must be in the format HH:MM.");
             RuleFor(x => x.FkRealEstateId)
                 .NotEmpty();
             RuleFor(x => x.FkAgentId)
